d639eb30ff80ac52ca6c786cca628b74bd1b87d5
MrAghyad <theriddler_214@hotmail.com>
2021-02-21 16:09:29 +0300

    Added Restaurant Edit Form

--- Pages/Restaurants/Edit.cshtml.cs
+++ Pages/Restaurants/Edit.cshtml.cs
@@ -6,19 +6,27 @@ using FoodProject.Core;
 using FoodProject.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace FoodProject.Pages.Restaurants
 {
     public class EditModel : PageModel
     {
         private readonly IRestaurantData restaurantData;
+        private readonly IHtmlHelper htmlHelper;
+
         public Restaurant Restaurant { get; set; }
-        public EditModel(IRestaurantData restaurantData)
+        public IEnumerable<SelectListItem> Cuisines { get; set; }
+        public EditModel(IRestaurantData restaurantData, IHtmlHelper htmlHelper)
         {
             this.restaurantData = restaurantData;
+            this.htmlHelper = htmlHelper;
         }
+
         public IActionResult OnGet(int restaurantId)
         {
+            Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
+            
             Restaurant = restaurantData.GetRestaurantById(restaurantId);
 
             if(Restaurant == null)
