e6cd13c51e0518e4869be1a504bb1f2ceee720fd
MrAghyad <theriddler_214@hotmail.com>
2021-03-14 19:12:48 +0300

    Added Db class

--- FoodProject.Data/IRestaurantData.cs
+++ FoodProject.Data/IRestaurantData.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using FoodProject.Core;
 
 namespace FoodProject.Data
@@ -10,54 +9,7 @@ namespace FoodProject.Data
         Restaurant GetRestaurantById(int id);
         Restaurant Update(Restaurant updatedRestaurant);
         Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
         int Commit();
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        List<Restaurant> restaurants;
-
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant{Id = 1, Name = "Pizza Hut", Location = "Jeddah", Cuisine = CuisineType.Italian},
-                new Restaurant{Id = 2, Name = "Taco Bell", Location = "Jeddah", Cuisine = CuisineType.Mexican},
-                new Restaurant{Id = 3, Name = "Tekka", Location = "Jeddah", Cuisine = CuisineType.Indian},
-            };
-        }
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
-        {
-            return from r in restaurants 
-                    where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                    orderby r.Name
-                    select r; 
-        }
-        public Restaurant GetRestaurantById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-        public Restaurant Add(Restaurant newRestaurant)
-        {
-            restaurants.Add(newRestaurant);
-            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
-
-            return newRestaurant;
-        }
-        public Restaurant Update(Restaurant updatedRestaurant)
-        {
-            var restaurant = restaurants.SingleOrDefault<Restaurant>(r => r.Id == updatedRestaurant.Id);
-            if(restaurant != null)
-            {
-                restaurant.Name = updatedRestaurant.Name;
-                restaurant.Location = updatedRestaurant.Location;
-                restaurant.Cuisine = updatedRestaurant.Cuisine;
-            }
-            return restaurant;
-        }
-        public int Commit()
-        {
-            return 0;
-        }
     }
 }
